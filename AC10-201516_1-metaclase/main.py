4b8b462f9d9c0c176dbc2eae1f25db97f33b15d7
JonnyLee <jonnylee719@gmail.com>
2016-04-21 11:54:08 -0300

    Terminado AC10

--- AC10-201516_1-metaclase/main.py
+++ AC10-201516_1-metaclase/main.py
@@ -1,7 +1,35 @@
 # debes definir la metaclase 'Meta' a continuacion
+def create_property(name, type):
+    def setter(self, val):
+        if isinstance(val, type):
+            self.__dict__[name] = val
+        else:
+            print('Wrong attribute type')
 
+    def getter(self):
+        return self.__dict__.get(name)
+
+    return property(getter, setter)
+
+
+class Meta(type):
+    def __new__(cls, name, bases, attrs):
+        for key, val in attrs.items():
+            if isinstance(val, type):
+                # Not protected attributes
+                attrs[key] = create_property(key, val)
+        return super().__new__(cls, name, bases, attrs)
 
 # debes definir las clases 'Person' y 'Company' a continuacion
+class Person(metaclass=Meta):
+    name = str
+    age = int
+
+
+class Company(metaclass=Meta):
+    name = str
+    stock_value = float
+    employee = list
 
 
 # El resto es para probar tu programa
