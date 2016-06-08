fd0f080814954d5b4084b1fc26977f102ba4c6d1
JonnyLee <jonnylee719@gmail.com>
2016-06-08 12:55:54 -0400

    AC23

--- AC_serializacion/main.py
+++ AC_serializacion/main.py
@@ -5,6 +5,7 @@ from PyQt4 import QtCore, QtGui
 import datetime
 import pickle
 import os
+import functools as func
 
 # Usted debe:
 # Escribir la clase Cliente
@@ -12,6 +13,36 @@ import os
 # Completar el método 'generarArchivo' de la clase 'VentanaAdmin'
 # Completar el método 'on_pushButton_clicked' de la clase 'Input'
 
+class Cliente:
+    def __init__(self, nombre, _id, dinero):
+        self.nombre = nombre
+        self._id = _id
+        self.dinero = dinero
+
+    def __setstate__(self, _dict):
+        self.__dict__ = _dict
+
+    def __getstate__(self):
+        state = self.__dict__.copy()
+        return state
+
+    def __eq__(self, obj):
+        if not obj:
+            return False
+        if not isinstance(obj, Cliente):
+            return False
+        if obj.nombre != self.nombre:
+            return False
+        if obj._id != self._id:
+            return False
+        if obj.dinero != self.dinero:
+            return False
+        return True
+
+    def __repr__(self):
+        print(self.__dict__)
+        return 'Cliente id: {s} \nNombre: {c.nombre} \nGastado: {c.dinero}'\
+            .format(s=123, c=self)
 
 class VentanaCajero(QtGui.QDialog):
 
@@ -43,12 +74,35 @@ class VentanaCajero(QtGui.QDialog):
         self.buttonBox.rejected.connect(self.close)
 
     def serializarCliente(self):
-        #####
+        fnames = os.listdir('ClientesDB')
+        print('Files: \n{}'.format(fnames))
+        existentes = {fn.split('.')[0]: fn for fn in fnames}
 
-        # Completar
-
-        #####
-
+        nombre = self.clienteText.text()
+        _id = self.idText.text()
+        gastado = self.gastadoText.text()
+        if not nombre or not _id or not gastado:
+            # si no tiene texto en cualquier edit text, no hace nada
+            return
+        nueva_entrada = Cliente(nombre, _id, gastado)
+        existe = False
+        if _id in existentes:
+            # cliente ya esta en la carpeta
+            fname = 'ClientesDB' + os.sep + existentes[_id]
+            with open(fname, 'rb') as file:
+                cliente = pickle.load(file)
+                print(cliente)
+                if nueva_entrada == cliente:
+                    existe = True
+                    # actualiza el dinero con nuevo gasto
+                    nueva_entrada.dinero += cliente.dinero
+            print(cliente)
+        if not existe:
+            fname = 'ClientesDB' + os.sep + _id + '.walkcart'
+        # actualiza el cliente y pickle dump el cliente
+        with open(fname, 'wb') as file:
+            print('Actualiza cliente archivo')
+            pickle.dump(nueva_entrada, file)
         self.clienteText.setText("")
         self.idText.setText("")
         self.gastadoText.setText("")
@@ -96,12 +150,15 @@ class Input(QtGui.QWidget):
 
     @QtCore.pyqtSlot()
     def on_pushButton_clicked(self):
-        #####
-
-        # Completar
-
-        #####
-        pass
+        # abrir el archivo 'cajeros.walkcart'
+        with open('cajeros.walkcart', 'rb') as file:
+            cajeros_lista = pickle.load(file)
+            print(cajeros_lista)
+            # Chaquear el texto en el widget self.userNameText
+            nombre = self.userNameText.text()
+            if nombre in cajeros_lista:
+                self.window = VentanaCajero()
+                self.window.show()
 
 
 if __name__ == "__main__":
